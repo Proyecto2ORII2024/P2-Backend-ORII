e4cbe1713ce747f61b836816706594ab387d22ff
rscruz <santiagocrzzz1711@gmail.com>
2024-10-25 17:06:40 -0500

    Add documentation

--- backend-orii/src/main/java/com/edu/unicauca/orii/core/user/infrastructure/adapters/input/rest/controller/EmailConfirmation.java
+++ backend-orii/src/main/java/com/edu/unicauca/orii/core/user/infrastructure/adapters/input/rest/controller/EmailConfirmation.java
@@ -5,16 +5,32 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.edu.unicauca.orii.core.user.application.ports.input.IEmailTokenInput;
 import lombok.RequiredArgsConstructor;
 
 @Controller
+/**
+ * REST controller for handling email confirmation functionalities, such as 
+ * sending confirmation emails and confirming email tokens.
+ * 
+ * This controller defines endpoints to initiate email confirmation 
+ * processes and to confirm email tokens.
+ */
+
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/email")
 public class EmailConfirmation {
 
     private final IEmailTokenInput emailTokenInput;
 
+       /**
+     * Confirms a user's email by validating a token.
+     * 
+     * @param token the confirmation token sent to the user's email
+     */
     @GetMapping("/confirmEmail/{token}")
     public String confirmEmail(@PathVariable String token, Model model) {
         model.addAttribute("isValid", emailTokenInput.confirmToken(token));
