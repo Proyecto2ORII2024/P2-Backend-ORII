40da9d969b74ea7b348f29a0e449eef397e74d06
JulianRuano <julianrmajin@gmail.com>
2024-11-01 14:07:08 -0500

    Refactor security settings and filters

--- backend-orii/src/main/java/com/edu/unicauca/orii/core/mobility/infrastructure/adapters/input/rest/controller/FormCommandController.java
+++ backend-orii/src/main/java/com/edu/unicauca/orii/core/mobility/infrastructure/adapters/input/rest/controller/FormCommandController.java
@@ -1,6 +1,7 @@
 package com.edu.unicauca.orii.core.mobility.infrastructure.adapters.input.rest.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -30,6 +31,7 @@ public class FormCommandController {
     private final IFormRestMapper formRestMapper;
 
     @PostMapping("/create")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<FormCreateResponse> createForm(@Valid @RequestBody FormCreateRequest formCreateRequest) {
         Form form = formRestMapper.toForm(formCreateRequest);
         form = formCommandService.createForm(form);
