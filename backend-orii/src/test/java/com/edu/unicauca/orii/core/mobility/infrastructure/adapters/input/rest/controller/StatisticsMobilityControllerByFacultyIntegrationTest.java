1bb8e4fbf79b9fd971507b0c5401496dcfa500ed
Jojan Esteban Serna <78891995+Jojan-Esteban-Serna@users.noreply.github.com>
2024-11-28 23:42:42 -0500

    Added statistics tests

--- backend-orii/src/test/java/com/edu/unicauca/orii/core/mobility/infrastructure/adapters/input/rest/controller/StatisticsMobilityControllerByFacultyIntegrationTest.java
+++ backend-orii/src/test/java/com/edu/unicauca/orii/core/mobility/infrastructure/adapters/input/rest/controller/StatisticsMobilityControllerByFacultyIntegrationTest.java
@@ -58,11 +58,15 @@ class StatisticsMobilityControllerByFacultyIntegrationTest extends BaseTest {
         return objectMapper.writeValueAsString(data);
     }
 
-    @AfterAll
-    void tearAll(){
+    @AfterEach
+    void tearDown(){
         this.formRepository.deleteAll();
+    }
+    @AfterAll
+    void tearDownAll(){
         this.agreementRepository.deleteAll();
     }
+
     @BeforeAll
     void setUp() {
         this.formRepository.deleteAll();
@@ -86,7 +90,7 @@ class StatisticsMobilityControllerByFacultyIntegrationTest extends BaseTest {
     }
 
     @Test
-    void testGetStatisticsByFaculty() throws Exception {
+    void testGetStatisticsByFacultyOutput() throws Exception {
 
         FormCreateRequest validData = FormCreateRequest.builder()
                 .orii(true)
@@ -142,6 +146,62 @@ class StatisticsMobilityControllerByFacultyIntegrationTest extends BaseTest {
                 .andExpect(jsonPath("$.input[0]").value(0));
     }
 
+    @Test
+    void testGetStatisticsByFacultyInput() throws Exception {
+
+        FormCreateRequest validData = FormCreateRequest.builder()
+                .orii(true)
+                .direction(DirectionEnum.INCOMING_IN_PERSON)
+                .gender("Male")
+                .cta(1)
+                .entryDate(new SimpleDateFormat("dd-MM-yyyy").parse("30-09-2024"))
+                .exitDate(new SimpleDateFormat("dd-MM-yyyy").parse("31-10-2024"))
+                .originProgram("Ingeniería de Sistemas")
+                .destinationProgram("Ciencia de Datos")
+                .city("Bogotá")
+                .country("Colombia")
+                .teacher("Dr. Juan Pérez")
+                .faculty(FacultyEnum.FIET)
+                .funding(BigDecimal.valueOf(2000))
+                .fundingSource("Beca Colciencias")
+                .destination("Universidad Nacional de Colombia")
+                .origin("Universidad del Cauca")
+                .agreementId(initialAgreementEntity.getAgreementId())
+                .event(EventRequest.builder()
+                        .description("Congreso Internacional de Inteligencia Artificial")
+                        .eventTypeId(initialEventTypeEntity.getEventTypeId())
+                        .build())
+                .person(PersonData.builder()
+                        .identificationType(IdentificationTypeEnum.CC)
+                        .personType(PersonTypeEnum.TEACHER)
+                        .firstName("Carlos")
+                        .lastName("Gómez")
+                        .identification("987654321")
+                        .email("carlos.gomez@unicauca.edu.co")
+                        .build())
+                .build();
+
+        mockMvc.perform(post("/form/create")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(toJson(validData)))
+                .andExpect(status().isCreated());
+
+        mockMvc.perform(get(ENDPOINT)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.faculty").exists())
+                .andExpect(jsonPath("$.input").exists())
+                .andExpect(jsonPath("$.output").exists())
+                .andExpect(jsonPath("$.faculty").isArray())
+                .andExpect(jsonPath("$.input").isArray())
+                .andExpect(jsonPath("$.output").isArray())
+                .andExpect(jsonPath("$.faculty").isNotEmpty())
+                .andExpect(jsonPath("$.input").isNotEmpty())
+                .andExpect(jsonPath("$.output").isNotEmpty())
+                .andExpect(jsonPath("$.faculty[0]").value("Facultad de Ingeniería Electrónica y Telecomunicaciones"))
+                .andExpect(jsonPath("$.output[0]").value(0))
+                .andExpect(jsonPath("$.input[0]").value(1));
+    }
     @Test
     @WithMockUser(username = "user", roles = {"USER"})
     void testGetStatisticsByFacultyWithoutPermission() throws Exception {
